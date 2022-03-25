778ff8f77089d8c8bdee1e508127ee4588037e0c
NekoInverter <chsqwyx@gmail.com>
2022-03-25 19:11:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Undo
+Add undo and redo to all EditText context menus.
+将撤消和重做添加到所有 EditText 上下文菜单。
---
+++ app/src/main/java/io/github/tehcneko/undo/UndoHook.java
@@ -0,0 +1,50 @@
+package io.github.tehcneko.undo;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class UndoHook implements IXposedHookLoadPackage {
+    private static final String TAG = "UndoHook";
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
+        try {
+            var RClass = XposedHelpers.findClass("com.android.internal.R$string", loadPackageParam.classLoader);
+            var undoId = XposedHelpers.getStaticIntField(RClass, "undo");
+            var redoId = XposedHelpers.getStaticIntField(RClass, "redo");
+            var textActionModeCallbackClass = XposedHelpers.findClass("android.widget.Editor$TextActionModeCallback", loadPackageParam.classLoader);
+            var editorClass = XposedHelpers.findClass("android.widget.Editor", loadPackageParam.classLoader);
+            var editorField = XposedHelpers.findField(textActionModeCallbackClass, "this$0");
+            var textViewField = XposedHelpers.findField(editorClass, "mTextView");
+            XposedHelpers.findAndHookMethod(
+                    textActionModeCallbackClass,
+                    "populateMenuWithItems",
+                    Menu.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            var editor = editorField.get(param.thisObject);
+                            var textView = textViewField.get(editor);
+                            var menu = (Menu) param.args[0];
+                            if ((boolean) XposedHelpers.callMethod(textView, "canUndo")) {
+                                menu.add(Menu.NONE, android.R.id.undo, 2, undoId)
+                                        .setAlphabeticShortcut('z')
+                                        .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+                            }
+                            if ((boolean) XposedHelpers.callMethod(textView, "canRedo")) {
+                                menu.add(Menu.NONE, android.R.id.redo, 3, redoId)
+                                        .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+                            }
+                        }
+                    });
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+        }
+    }
+}
